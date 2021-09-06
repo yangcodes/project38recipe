f770905175f5aa3ba0b9c73fb6d93099aeb72635
Jenny Lin <misslinyang91@gmail.com>
2021-09-06 19:17:20 +1000

    display the recipes

--- app.js
+++ app.js
@@ -32,5 +32,33 @@ async function fetchRecipes(searchQuery) {
     `${recipeURL}${searchQuery}&app_id=${appId}&app_key=${appKey}&to=30`
   );
   const responseData = await response.json();
-  console.log(responseData.hits);
+  displayRecipes(responseData.hits);
+}
+
+function displayRecipes(recipeResults) {
+  let recipeEl = "";
+  recipeResults.forEach((recipeResult) => {
+    recipeEl += `
+    <div class="item">
+      <img src="${recipeResult.recipe.image}" />
+      <div class="content-wrapper">
+        <h2 class="recipe-title">${recipeResult.recipe.label}</h2>
+        <a href="${
+          recipeResult.recipe.url
+        }" target="_blank" class="view-recipe">View Recipe</a>
+      </div>
+      <div class="recipe-desc">
+        <p class="item-data">Calories: ${recipeResult.recipe.calories.toFixed(
+          2
+        )}</p>
+        <p class="item-data">Diet Label: ${recipeResult.recipe.dietLabels}</p>
+        <p class="item-data">Health Label: ${
+          recipeResult.recipe.healthLabels
+        }</p>
+        <p class="item-data">Source: ${recipeResult.recipe.source}</p>
+      </div>
+  </div>
+    `;
+    searchResults.innerHTML = recipeEl;
+  });
 }
